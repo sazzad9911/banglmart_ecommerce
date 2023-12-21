947ddd625d0e1bb4729ff2bbaa5a971457bb2d1e
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2023-12-22 00:12:11 +0600

    dfd

--- functions/gamesFunction.js
+++ functions/gamesFunction.js
@@ -20,3 +20,22 @@ export const getGameList = async (req, res) => {
     res.status(StatusCodes.EXPECTATION_FAILED).json({ message: e.message });
   }
 };
+export const getGameListOffline = async (req, res) => {
+    try {
+      const list = await axios.post(
+        "https://stage.game-program.com/api/seamless/provider",
+        {
+          api_password: "3XKBmgmYXAKpcuVyQv",
+          api_login: "40xbet_mc_s",
+          method: "getGameList",
+          show_additional: true,
+          show_systems: 1,
+          currency: "USD",
+        }
+      );
+      //console.log(list.data);
+      res.status(StatusCodes.OK).json(list.data);
+    } catch (e) {
+      res.status(StatusCodes.EXPECTATION_FAILED).json({ message: e.message });
+    }
+  };
