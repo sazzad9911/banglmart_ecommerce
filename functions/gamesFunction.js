9309a2f05a4246d35b06bbafb1f5f17be4a4ed91
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-02-01 14:33:29 +0600

    sda

--- functions/gamesFunction.js
+++ functions/gamesFunction.js
@@ -21,21 +21,44 @@ export const getGameList = async (req, res) => {
   }
 };
 export const getGameListOffline = async (req, res) => {
-    try {
-      const list = await axios.post(
-        "https://stage.game-program.com/api/seamless/provider",
-        {
-          api_password: "3XKBmgmYXAKpcuVyQv",
-          api_login: "40xbet_mc_s",
-          method: "getGameList",
-          show_additional: true,
-          show_systems: 1,
-          currency: "USD",
-        }
-      );
-      //console.log(list.data);
-      res.status(StatusCodes.OK).json(list.data);
-    } catch (e) {
-      res.status(StatusCodes.EXPECTATION_FAILED).json({ message: e.message });
-    }
-  };
+  try {
+    const list = await axios.post(
+      "https://stage.game-program.com/api/seamless/provider",
+      {
+        api_password: "3XKBmgmYXAKpcuVyQv",
+        api_login: "40xbet_mc_s",
+        method: "getGameList",
+        show_additional: true,
+        show_systems: 1,
+        currency: "USD",
+      }
+    );
+    //console.log(list.data);
+    res.status(StatusCodes.OK).json(list.data);
+  } catch (e) {
+    res.status(StatusCodes.EXPECTATION_FAILED).json({ message: e.message });
+  }
+};
+export const getGame = async (req, res) => {
+  const {id}=req.params
+  try {
+    const list = await axios.post(
+      "https://stage.game-program.com/api/seamless/provider",
+      {
+        api_password: "3XKBmgmYXAKpcuVyQv",
+        api_login: "40xbet_mc_s",
+        method: "getGame",
+        lang: "EN",
+        user_username: "sazzad",
+        user_password: "sazzad#991",
+        gameid: id,
+        play_for_fun: 0,
+        currency: "USD",
+      }
+    );
+    //console.log(list.data);
+    res.status(StatusCodes.OK).json(list.data);
+  } catch (e) {
+    res.status(StatusCodes.EXPECTATION_FAILED).json({ message: e.message });
+  }
+};
--- routes/games.js
+++ routes/games.js
@@ -3,9 +3,10 @@ import rateLimiter from "express-rate-limit";
 import verifyUser from "../middleware/verifyUser.js";
 import upload from "../lib/upload.js";
 import { createConversation, deleteConversation, getConversation, getMessage, getSingleConversation, getUnRead, sendMessage } from "../functions/messageFunnction.js";
-import { getGameList } from "../functions/gamesFunction.js";
+import { getGame, getGameList } from "../functions/gamesFunction.js";
 
 const games = express.Router();
 
 games.route("/get").get(getGameList);
+games.route("/get/game/:id").get(getGame);
 export default games;
