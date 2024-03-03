484961d36bc9741d9d9a63badfc6a824901c6e4c
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-03-03 11:27:07 +0600

    fds

--- commands.s
+++ commands.s
@@ -1,2 +1,15 @@
 npx prisma generate   -> generate new schemes
 npx prisma migrate dev --name tags-model
+
+
+ cd server
+          git pull origin main
+          cd admin
+          npm i
+          npm run build
+          cd ..
+          cd frontend
+          npm i
+          npm run build
+          cd ..
+          npm i
