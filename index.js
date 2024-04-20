3981f121bf32fe4711dada567ce0aa550f8e1f1e
Sazzad Hossain <57707331+sazzad9911@users.noreply.github.com>
2024-04-20 09:49:15 +0600

    sd

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cors())
-//app.use(bodyParser.json());
+//app.use(bodyParser.json()); 
 //app.use(bodyParser.urlencoded({ extended: true }));
  
 //app.use(helmet({crossOriginResourcePolicy:false}));
